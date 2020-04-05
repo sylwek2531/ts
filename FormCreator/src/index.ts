68a61265d66b5b74a6fe9741686fb101193b06b3
Sylwester Sztajner <sztajner_sylwester@ma-al.com>
2020-04-05 09:34:44 +0200

    create formcreator project , catching up

--- FormCreator/src/index.ts
+++ FormCreator/src/index.ts
@@ -1,1 +1,90 @@
 console.log("test");
+
+enum FieldType {
+    "inputText",
+    "textarea",
+    "inputDate",
+    "inputEmail",
+    "inputCheckbox",
+    "select"
+}
+
+interface Field {
+    name: string,
+    label: FieldLabel,
+    type: FieldType,
+    value: string,
+    render(): HTMLElement
+}
+
+class FieldLabel {
+    label: string = "label";
+    constructor(label: string) {
+        this.label = label ? label : this.label
+    }
+    render(): HTMLLabelElement {
+        const label = document.createElement("label");
+        label.innerText = this.label;
+        return label;
+    }
+}
+class InputField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+
+}
+class TextAreaField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+class DateField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+class EmailField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+class SelectField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+class CheckboxField implements Field{
+    name: string;
+    label: FieldLabel;
+    type: FieldType;
+    value: string;
+    render(): HTMLElement {
+        throw new Error("Method not implemented.");
+    }
+
+}
